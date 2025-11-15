3ad980f7b7395ff1ccc5f3618354bc4cd772ab60
josegavilanes12 <5m1l33y@5m1l33ys-mbp.clients.haverford.edu>
2025-11-15 00:05:09 -0500

    Adding logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Menu from "./pages/Menu/Menu";
 import About from "./pages/About/About";
+import Metrics from "./pages/Metrics/Metrics";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/about" element={<About />} />
+        <Route path="/metrics" element={<Metrics />} />
       </Routes>
     </>
   );
