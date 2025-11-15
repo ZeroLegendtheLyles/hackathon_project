220f1b6a3834727e9b88503f8fbc80fb4d81b000
josegavilanes12 <5m1l33y@5m1l33ys-mbp.clients.haverford.edu>
2025-11-14 21:19:09 -0500

    Adding Navbar and base for components

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div>
+        <img src=""></img>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>About</li>
+        <li>Shop</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
